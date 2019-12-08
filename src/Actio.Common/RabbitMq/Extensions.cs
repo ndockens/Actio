95db9035b072823ec7b377463745dc42a309a785
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2019-12-07 20:20:34 -0600

    Implemented ServiceHost and helper methods

---
+++ src/Actio.Common/RabbitMq/Extensions.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using System.Reflection;
+using RawRabbit;
+using Actio.Common.Commands;
+using Actio.Common.Events;
+
+namespace Actio.Common.RabbitMq
+{
+    public static class Extensions
+    {
+        public static Task WithCommandHandlerAsync<TCommand>(this IBusClient bus,
+            ICommandHandler<TCommand> handler) where TCommand : ICommand
+        {
+            return bus.SubscribeAsync<TCommand>(msg => handler.HandleAsync(msg),
+                ctx => ctx.UseSubscribeConfiguration(cgf => cgf.FromDeclaredQueue(q => q.WithName(GetQueueName<TCommand>()))));
+        }
+        public static Task WithEventHandlerAsync<TEvent>(this IBusClient bus,
+            IEventHandler<TEvent> handler) where TEvent : IEvent
+        {
+            return bus.SubscribeAsync<TEvent>(msg => handler.HandleAsync(msg),
+                ctx => ctx.UseSubscribeConfiguration(cgf => cgf.FromDeclaredQueue(q => q.WithName(GetQueueName<TEvent>()))));
+        }
+
+        private static string GetQueueName<T>()
+        {
+            return $"{Assembly.GetEntryAssembly().GetName()}/{typeof(T).Name}";
+        }
+    }
+}
