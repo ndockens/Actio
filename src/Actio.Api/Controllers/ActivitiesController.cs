887b01dd6e187ac179fe19e67deda179d5a3d38b
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2019-12-10 22:23:46 -0600

    Implemented API endpoints

---
+++ src/Actio.Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
+using Actio.Common.Commands;
+
+namespace Actio.Api.Controllers
+{
+    [Route("[controller")]
+    public class ActivitiesController : Controller
+    {
+        private readonly IBusClient _busClient;
+
+        public ActivitiesController(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Post([FromBody]CreateActivity command)
+        {
+            command.Id = Guid.NewGuid();
+            command.CreatedAt = DateTime.UtcNow;
+            await _busClient.PublishAsync(command);
+
+            return Accepted($"activities/{command.Id}");
+        }
+    }
+}
---
+++ src/Actio.Api/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
+using Actio.Common.Commands;
+
+namespace Actio.Api.Controllers
+{
+    [Route("[controller")]
+    public class UsersController : Controller
+    {
+        private readonly IBusClient _busClient;
+
+        public UsersController(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Post([FromBody]CreateUser command)
+        {
+            await _busClient.PublishAsync(command);
+
+            return Accepted();
+        }
+    }
+}
